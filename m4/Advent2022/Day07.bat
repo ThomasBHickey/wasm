f2fab3919fe0c331fe8d62dcf16c6c52cb14f22f
ThomasBHickey <ThomasBHickey@gmail.com>
2022-12-30 16:16:12 -0500

    Trouble with full input

--- m4/Advent2022/Day07.bat
+++ m4/Advent2022/Day07.bat
@@ -1,3 +1,4 @@
 m4 Day07.m4 >Day07.wat
 #wasmtime run Day07.wat --invoke _Day07a < $1
-wasmer run Day07.wat -i _Day07a < Day07.test
+#wasmer run Day07.wat -i _Day07a < Day07.test
+wasmer run Day07.wat -i _Day07a < Day07.input
